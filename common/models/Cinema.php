10c381b25229cba110f91e8bdc6e4783fe9a48e5
Pavel <pavlinter@gmail.com>
2014-05-28 22:10:21 +0300

    test2

--- common/models/Cinema.php
+++ common/models/Cinema.php
@@ -39,7 +39,7 @@ class Cinema extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => Yii::t('app', 'ID'),
+            'id' => Yii::t('app', 'ddID'),
             'name' => Yii::t('app', 'Surname'),
         ];
     }
